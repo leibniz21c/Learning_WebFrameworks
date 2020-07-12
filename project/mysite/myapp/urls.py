8fd908f4d562fc774b87c1bb7467a8fe53e19479
Heesung Yang <38760913+ndo04343@users.noreply.github.com>
2020-07-12 19:14:37 +0900

    2020.07.12.19:14

--- project/mysite/myapp/urls.py
+++ project/mysite/myapp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'myapp'
+
 urlpatterns = [
-    path('', views.index),
-    path('questions/', views.index_question, name='index'),
-    path('questions/<int:question_id>', views.detail, name='detail'),
+    path('', views.index, name='index'),
+    path('questions/', views.index_question, name='question_list'),
+    path('questions/<int:question_id>', views.detail, name='question_detail'),
 ]
