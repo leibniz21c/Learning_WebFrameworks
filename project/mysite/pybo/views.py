c7373bac18937465bf307358ae91a8d439bb4c03
Heesung Yang <38760913+ndo04343@users.noreply.github.com>
2020-07-14 20:28:33 +0900

    bootstrap

--- project/mysite/pybo/views.py
+++ project/mysite/pybo/views.py
@@ -8,7 +8,7 @@ from .models import Question
 def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list':question_list}
-    return render(request, 'myapp/index.html', context)
+    return render(request, 'myapp/question_list.html', context)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
